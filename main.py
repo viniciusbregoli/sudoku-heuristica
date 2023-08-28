b2871ee085da93107b71f4493659085ccb50d383
Bregoli <viniciusbregoli@hotmail.com>
2023-08-28 19:35:08 -0300

    Changed variable names

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ pygame.init()
 pygame.display.set_caption("Sudoku")
 
 # CONSTANTES =======================================================================================
-BRANCO = (255, 255, 255)
-PRETO = (0, 0, 0)
-CINZA = (200, 200, 200)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
 DARKER_GRAY = (210, 210, 210)
 
 BUTTON_WIDTH = 280  # Largura dos botões
@@ -18,13 +18,13 @@ BUTTON_HEIGHT = 60  # Altura dos botões
 BUTTON_MARGIN = 20  # Espaço entre os botões
 
 
-TAMANHO_BLOCO = 600 // 9
+CELL_SIZE = 600 // 9
 
-ALTURA = 9 * TAMANHO_BLOCO + BUTTON_MARGIN * 2 + BUTTON_HEIGHT
-LARGURA = 9 * TAMANHO_BLOCO
+HEIGHT = 9 * CELL_SIZE + BUTTON_MARGIN * 2 + BUTTON_HEIGHT
+WIDTH = 9 * CELL_SIZE
 # ===================================================================================================
 
-screen = pygame.display.set_mode((LARGURA, ALTURA))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 font = pygame.font.SysFont(None, 55)
 
 # Funções ===========================================================================================
@@ -35,18 +35,19 @@ def get_block_start(row, col):
 
 
 def draw_grid():
-    for x in range(0, LARGURA, TAMANHO_BLOCO):
-        pygame.draw.line(screen, CINZA, (x, 0), (x, 9 * TAMANHO_BLOCO))
-    for y in range(0, 9 * TAMANHO_BLOCO, TAMANHO_BLOCO):
-        pygame.draw.line(screen, CINZA, (0, y), (LARGURA, y))
+    # Draw thinner lines
+    for x in range(0, WIDTH, CELL_SIZE):
+        pygame.draw.line(screen, GRAY, (x, 0), (x, 9 * CELL_SIZE))
+    for y in range(0, 9 * CELL_SIZE, CELL_SIZE):
+        pygame.draw.line(screen, GRAY, (0, y), (WIDTH, y))
 
-    # Desenhar as linhas mais grossas para os quadrados 3x3
-    for x in range(0, LARGURA, 3 * TAMANHO_BLOCO):
-        pygame.draw.line(screen, PRETO, (x, 0), (x, 9 * TAMANHO_BLOCO), 3)
-    for y in range(0, 9 * TAMANHO_BLOCO, 3 * TAMANHO_BLOCO):
-        pygame.draw.line(screen, PRETO, (0, y), (LARGURA, y), 3)
+    # Draw thicker lines
+    for x in range(0, WIDTH, 3 * CELL_SIZE):
+        pygame.draw.line(screen, BLACK, (x, 0), (x, 9 * CELL_SIZE), 3)
+    for y in range(0, 9 * CELL_SIZE, 3 * CELL_SIZE):
+        pygame.draw.line(screen, BLACK, (0, y), (WIDTH, y), 3)
     pygame.draw.line(
-        screen, PRETO, (0, 9 * TAMANHO_BLOCO), (LARGURA, 9 * TAMANHO_BLOCO), 3
+        screen, BLACK, (0, 9 * CELL_SIZE), (WIDTH, 9 * CELL_SIZE), 3
     )
 
 
@@ -57,34 +58,34 @@ def draw_numbers():
                 if initial_positions and (row, col) in initial_positions:
                     color = (0, 0, 255)
                 else:
-                    color = PRETO
+                    color = BLACK
                 pygame.draw.rect(
                     screen,
-                    BRANCO,
+                    WHITE,
                     (
-                        col * TAMANHO_BLOCO,
-                        row * TAMANHO_BLOCO,
-                        TAMANHO_BLOCO,
-                        TAMANHO_BLOCO,
+                        col * CELL_SIZE,
+                        row * CELL_SIZE,
+                        CELL_SIZE,
+                        CELL_SIZE,
                     ),
-                )  # Limpa a área antes de desenhar
+                )  # Clears the cell before drawing the number
                 num_text = font.render(str(tabuleiro[row][col]), True, color)
                 screen.blit(
-                    num_text, (col * TAMANHO_BLOCO + 15, row * TAMANHO_BLOCO + 10)
+                    num_text, (col * CELL_SIZE + 15, row * CELL_SIZE + 10)
                 )
 
 
 def draw_buttons():
     button1_rect = pygame.Rect(
-        (LARGURA - 2 * BUTTON_WIDTH - 1.5 * BUTTON_MARGIN)
-        // 2,  # Ajuste no posicionamento x
-        9 * TAMANHO_BLOCO + BUTTON_MARGIN,
+        (WIDTH - 2 * BUTTON_WIDTH - 1.5 * BUTTON_MARGIN)
+        // 2,  # Ajust in x position
+        9 * CELL_SIZE + BUTTON_MARGIN,
         BUTTON_WIDTH,
         BUTTON_HEIGHT,
     )
     button2_rect = pygame.Rect(
-        (LARGURA + 0.5 * BUTTON_MARGIN) // 2,  # Ajuste no posicionamento x
-        9 * TAMANHO_BLOCO + BUTTON_MARGIN,
+        (WIDTH + 0.5 * BUTTON_MARGIN) // 2,  # Ajust in x position
+        9 * CELL_SIZE + BUTTON_MARGIN,
         BUTTON_WIDTH,
         BUTTON_HEIGHT,
     )
@@ -93,10 +94,10 @@ def draw_buttons():
     pygame.draw.rect(screen, DARKER_GRAY, button2_rect)
 
     font = pygame.font.SysFont(None, 30)
-    button1_text = font.render("Resolver por Busca Gulosa", True, PRETO)
-    button2_text = font.render("Resolver por A*", True, PRETO)
+    button1_text = font.render("Resolver por Busca Gulosa", True, BLACK)
+    button2_text = font.render("Resolver por A*", True, BLACK)
 
-    # Centralizando o texto dentro do botão
+    # Centralizing the text
     button1_text_pos = (
         button1_rect.centerx - button1_text.get_width() // 2,
         button1_rect.centery - button1_text.get_height() // 2,
@@ -118,25 +119,25 @@ def draw_highlights():
     block_start_row, block_start_col = get_block_start(row, col)
 
     pygame.draw.rect(
-        screen, DARKER_GRAY, (0, row * TAMANHO_BLOCO, 9 * TAMANHO_BLOCO, TAMANHO_BLOCO)
+        screen, DARKER_GRAY, (0, row * CELL_SIZE, 9 * CELL_SIZE, CELL_SIZE)
     )
 
-    # Destacando a coluna
+    # Highlighting the column
     pygame.draw.rect(
-        screen, DARKER_GRAY, (col * TAMANHO_BLOCO, 0, TAMANHO_BLOCO, 9 * TAMANHO_BLOCO)
+        screen, DARKER_GRAY, (col * CELL_SIZE, 0, CELL_SIZE, 9 * CELL_SIZE)
     )
 
-    # Destacando o bloco 3x3
+    # Highlighting the block
     for i in range(3):
         for j in range(3):
             pygame.draw.rect(
                 screen,
                 DARKER_GRAY,
                 (
-                    (block_start_col + j) * TAMANHO_BLOCO,
-                    (block_start_row + i) * TAMANHO_BLOCO,
-                    TAMANHO_BLOCO,
-                    TAMANHO_BLOCO,
+                    (block_start_col + j) * CELL_SIZE,
+                    (block_start_row + i) * CELL_SIZE,
+                    CELL_SIZE,
+                    CELL_SIZE,
                 ),
             )
 
@@ -165,17 +166,17 @@ def solve(board):
 
 
 def is_valid(board, num, row, col):
-    # Verifica linha
+    # Verify row
     for i in range(9):
         if board[row][i] == num:
             return False
 
-    # Verifica coluna
+    # Verify column
     for i in range(9):
         if board[i][col] == num:
             return False
 
-    # Verifica quadrado 3x3
+    # Verify block
     start_row, start_col = 3 * (row // 3), 3 * (col // 3)
     for i in range(3):
         for j in range(3):
@@ -200,18 +201,16 @@ def generate_sudoku():
     solve(board)
     return board
 
-
 tabuleiro = generate_sudoku()
 
-
 def remove_numbers(board, num_to_remove=30):
-    # Criar uma lista de todas as células
+    # Create a list of all cells
     cells = [(i, j) for i in range(9) for j in range(9)]
 
-    # Embaralhar a lista
+    # Shuffle the list
     random.shuffle(cells)
 
-    # Remover os primeiros 'num_to_remove' números
+    # Remove the numbers one by one
     for i in range(num_to_remove):
         row, col = cells[i]
         board[row][col] = 0
@@ -231,17 +230,17 @@ def greedy_search(board):
     if not possibilities:
         return False
 
-    # Ordena os números por menor quantidade de possibilidades
+    # Orders the possibilities by the number of possibilities for each cell
     possibilities.sort(key=lambda num: len(get_possibilities(board, row, col, num)))
 
     for num in possibilities:
         if is_valid(board, num, row, col):
             board[row][col] = num
-            pygame.time.wait(25)  # Adiciona um atraso para visualização
-            draw_numbers()  # Redesenha os números no tabuleiro
-            draw_grid()  # Redesenha a grade
-            check_for_quit()  # Verifica se o usuário clicou no botão de fechar
-            pygame.display.flip()  # Atualiza o display
+            pygame.time.wait(25)  # Adds a delay for visualization
+            draw_numbers()  # Redraws the numbers on the board
+            draw_grid()  # Redraws the grid
+            check_for_quit()  # Verify if the user clicked on the close button
+            pygame.display.flip()  # Updates the display
 
             if greedy_search(board):
                 return True
@@ -251,7 +250,7 @@ def greedy_search(board):
 
 
 def get_possibilities(board, row, col, current_number=None):
-    # Retorna uma lista dos números possíveis para uma célula específica
+    # Returns a list of all possible numbers for a given cell
     possibilities = []
     for num in range(1, 10):
         if num == current_number:
@@ -293,40 +292,25 @@ def a_star(board):
         for i in range(9):
             for j in range(9):
                 board[i][j] = current_board[i][j]
-        pygame.time.wait(2)  # Adiciona um atraso para visualização
-        draw_numbers()  # Redesenha os números no tabuleiro
-        draw_grid()  # Redesenha a grade
-        check_for_quit()  # Verifica se o usuário clicou no botão de fechar
-        pygame.display.flip()  # Atualiza o display
+        pygame.time.wait(25)  # Adds a delay for visualization
+        draw_numbers()  # Redraws the numbers on the board
+        draw_grid()  # Redraws the grid
+        check_for_quit()  # Verify if the user clicked on the close button
+        pygame.display.flip()  # Updates the display
         if h(current_board) == 0: 
             return
         row, col = find_empty_cell(current_board)
         for num in range(1, 10):
-            if is_valid_move(current_board, row, col, num):
+            if is_valid(current_board,num, row, col):
                 new_board = [row[:] for row in current_board]
                 new_board[row][col] = num
                 heapq.heappush(priority_queue, (g(new_board) + h(new_board), new_board))
 
-
-def is_valid_move(board, row, col, num):
-    for x in range(9):
-        if board[row][x] == num:
-            return False
-    for x in range(9):
-        if board[x][col] == num:
-            return False
-    startRow, startCol = 3 * (row // 3), 3 * (col // 3)
-    for i in range(3):
-        for j in range(3):
-            if board[i + startRow][j + startCol] == num:
-                return False
-    return True
-
-# Função para calcular g(n) - número de células preenchidas
+# Function to calculate g(n) - number of filled cells
 def g(board):
     return sum(cell != 0 for row in board for cell in row)
 
-# Função para calcular h(n) - número de células vazias
+# Function to calculate h(n) - number of empty cells
 def h(board):
     return sum(cell == 0 for row in board for cell in row)
 
@@ -334,14 +318,14 @@ def game_loop():
     global selected_cell
     global original_board
     button1_rect = pygame.Rect(
-        (LARGURA - 2 * BUTTON_WIDTH - BUTTON_MARGIN) // 2,
-        9 * TAMANHO_BLOCO + BUTTON_MARGIN,
+        (WIDTH - 2 * BUTTON_WIDTH - BUTTON_MARGIN) // 2,
+        9 * CELL_SIZE + BUTTON_MARGIN,
         BUTTON_WIDTH,
         BUTTON_HEIGHT,
     )
     button2_rect = pygame.Rect(
-        (LARGURA + BUTTON_MARGIN) // 2,
-        9 * TAMANHO_BLOCO + BUTTON_MARGIN,
+        (WIDTH + BUTTON_MARGIN) // 2,
+        9 * CELL_SIZE + BUTTON_MARGIN,
         BUTTON_WIDTH,
         BUTTON_HEIGHT,
     )
@@ -362,35 +346,33 @@ def game_loop():
                     a_star(tabuleiro)
                 else:
                     cell_row, cell_col = (
-                        mouse_y // TAMANHO_BLOCO,
-                        mouse_x // TAMANHO_BLOCO,
+                        mouse_y // CELL_SIZE,
+                        mouse_x // CELL_SIZE,
                     )
 
-                    # Verifique se as coordenadas estão dentro da grade
+                    # Verify if the click was inside the board
                     if 0 <= cell_row < 9 and 0 <= cell_col < 9:
-                        # Selecione a célula se ela estiver vazia
+                        # Select the cell if it's empty
                         if tabuleiro[cell_row][cell_col] == 0:
                             selected_cell = (cell_row, cell_col)
                         else:
-                            # Desmarque a seleção atual se uma célula não vazia for clicada
+                            # Unselect the cell if it's already selected
                             selected_cell = None
 
             if event.type == pygame.KEYDOWN:
                 if event.unicode.isdigit() and selected_cell:
                     row, col = selected_cell
-                    # Verifique se o número é fixo
+                    # Verify if the number is fixed
                     if not fixed_numbers[row][col]:
                         num = int(event.unicode)
                         if is_valid(tabuleiro, num, row, col):
                             tabuleiro[row][col] = num
                             selected_cell = None
-
-        screen.fill(BRANCO)
+        screen.fill(WHITE)
         draw_numbers()
         draw_grid()
         draw_highlights()
         draw_buttons()
-
         pygame.display.flip()
 
 
